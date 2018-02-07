88d8cd709f4f267dea78143c7e62feab51868bcf
Bhirawa Mbani <bhirmbani@gmail.com>
2018-02-08 06:40:07 +0700

    clean up

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Login from '@/components/Login';
-// import HelloWorld from '@/components/HelloWorld';
+import Login from '../components/Login/Login';
 
 Vue.use(Router);
 
