e06d3dcf139a36e9911e329e4a61794325c6e3bb
Bhirawa Mbani <bhirmbani@gmail.com>
2018-02-08 14:24:21 +0700

    init first data from store in table

--- src/components/UserList/UserList.vue
+++ src/components/UserList/UserList.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <h1>Test</h1>
+    <data-tables :data="users">
+      <el-table-column
+        v-for="title in titles"
+        :prop="title.prop"
+        :label="title.label"
+        :key="title.label"
+        sortable="custom">
+    </el-table-column>
+  </data-tables>
   </div>
 </template>
 
@@ -10,15 +18,29 @@ import { mapGetters } from 'vuex';
 export default {
   name: 'UserList',
   data() {
-    return {};
+    return {
+      titles: [
+        {
+          prop: 'username',
+          label: 'username',
+        },
+        {
+          prop: 'role',
+          label: 'role',
+        },
+      ],
+    };
   },
   methods: {
     async initUser() {
-      await this.$store.dispatch('gets');
+      const res = await this.$store.dispatch('gets');
     },
   },
-  created() {
-    this.initUser();
+  mounted() {
+    const init = this.initUser();
+  },
+  computed: {
+    ...mapGetters(['users']),
   },
 };
 </script>
