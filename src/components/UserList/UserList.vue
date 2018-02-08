7233507a11ea7c4ca94070ccf0a1946fce24cd88
Bhirawa Mbani <bhirmbani@gmail.com>
2018-02-08 13:33:09 +0700

    init user list component

---
+++ src/components/UserList/UserList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Test</h1>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'UserList',
+  data() {
+    return {};
+  },
+  methods: {
+    async initUser() {
+      await this.$store.dispatch('gets');
+    },
+  },
+  created() {
+    this.initUser();
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
