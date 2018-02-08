3ab15e866ff72f59cfeb21222a0b05b5f3f7703b
Bhirawa Mbani <bhirmbani@gmail.com>
2018-02-08 13:30:54 +0700

    init datatable to main

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import SuiVue from 'semantic-ui-vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import lang from 'element-ui/lib/locale/lang/en';
+import locale from 'element-ui/lib/locale';
+import DataTables from 'vue-data-tables';
 import Notifications from 'vue-notification';
 import store from './store';
 import App from './App';
 import router from './router';
 
+locale.use(lang);
+Vue.use(ElementUI);
+Vue.use(DataTables);
 Vue.use(SuiVue);
 Vue.use(Notifications);
 Vue.config.productionTip = false;
