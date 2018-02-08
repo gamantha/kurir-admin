ec16c32d6049ea2dc1dc3e69463ad500619438b3
Bhirawa Mbani <bhirmbani@gmail.com>
2018-02-08 13:30:19 +0700

    refactor helpers file

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,20 @@
-export const baseUrl = {
-  dev: 'http://localhost:3000/',
-};
+import axios from 'axios';
+import VueCookie from 'vue-cookie';
 
-export const test = {};
+const Authorization = VueCookie.get('token');
+
+class Helpers {
+  constructor() {
+    this.allInstance = axios.create({
+      baseURL: 'http://localhost:3000/',
+    });
+    this.authInstance = axios.create({
+      baseURL: 'http://localhost:3000/',
+      headers: { Authorization },
+    });
+  }
+}
+
+const helpers = new Helpers();
+
+export default helpers;
