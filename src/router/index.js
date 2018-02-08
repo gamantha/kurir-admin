b740a81c60673c19d965ca3fb7af5e449ce3f1b7
Bhirawa Mbani <bhirmbani@gmail.com>
2018-02-08 13:31:40 +0700

    add userlist route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Login from '../components/Login/Login';
+import UserList from '../components/UserList/UserList';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Login',
       component: Login,
+    },
+    {
+      path: '/user-list',
+      name: 'UserList',
+      component: UserList,
     },
   ],
 });
