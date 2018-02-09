f691ea493711b74ad0b0737e0b675ae989cec4eb
Bhirawa Mbani <bhirmbani@gmail.com>
2018-02-09 18:57:43 +0700

    update store

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import createLogger from 'vuex/dist/logger';
 import loginState from './modules/login';
-import userState from './modules/userList';
+import proposalState from './modules/proposalList';
 
 Vue.use(Vuex);
 
@@ -11,7 +11,7 @@ const debug = process.env.NODE_ENV !== 'production';
 export default new Vuex.Store({
   modules: {
     loginState,
-    userState,
+    proposalState,
   },
   strict: debug,
   plugins: debug ? [createLogger()] : [],
