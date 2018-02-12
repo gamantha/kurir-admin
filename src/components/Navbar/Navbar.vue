8255cb3fb9acc81288b507a6b756ea8819cb21e0
Bhirawa Mbani <bhirmbani@gmail.com>
2018-02-12 13:57:15 +0700

    init navbar component

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,9 +1,45 @@
 <template>
+  <div>
+    <div class="line"></div>
+    <el-menu
+    :default-active="activeIdx"
+    class="el-menu-demo"
+    mode="horizontal"
+    @select="handleSelect"
+    router="router"
   
+      >
+      <el-menu-item index="proposal">
+        <router-link to="proposal">Proposal List</router-link>
+      </el-menu-item>
+      <el-menu-item index="user">
+        <router-link to="user">User List</router-link>
+      </el-menu-item>
+      <el-menu-item index="/">
+        <router-link to="/">Back to Login</router-link>
+      </el-menu-item>
+    </el-menu>
+  </div>
+
 </template>
 
 <script>
-  
+import router from '../../router';
+
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+      activeIdx: 'proposal',
+      router: true,
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      this.activeIdx = key;
+    },
+  },
+};
 </script>
 
 <style>
