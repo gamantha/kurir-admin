7a9ec848c1c5ee4bae2447190f63270b9c2111fa
Bhirawa Mbani <bhirmbani@gmail.com>
2018-02-13 16:40:02 +0700

    fixing axios get request

--- src/store/modules/userList/actions.js
+++ src/store/modules/userList/actions.js
@@ -1,11 +1,14 @@
+import VueCookie from 'vue-cookie';
 import Helpers from '../../../helpers';
 
 const helpers = new Helpers();
 
-export const gets = async (auth) => {
+export const gets = async () => {
   try {
-    const result = await helpers.allInstance.get('api/user/', {
-      headers: auth,
+    const result = await helpers.instance.get('api/user/', {
+      headers: {
+        Authorization: `bearer ${VueCookie.get('token')}`,
+      },
     });
     return result.data;
   } catch (error) {
