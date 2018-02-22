39e3acfb6073767943af80cd8c14cb54f7931143
andy-shi88 <shi88.andy@gmail.com>
2018-02-22 14:41:15 +0700

    #9 fix lint

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,10 +1,10 @@
 import VueCookie from 'vue-cookie';
 
 export default (to, from, next) => {
-	const token = VueCookie.get('token');
-	if (token) {
-		next();
-	} else {
-		next({ path: '/' })
-	}
-}
+  const token = VueCookie.get('token');
+  if (token) {
+    next();
+  } else {
+    next({ path: '/' });
+  }
+};
