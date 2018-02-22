ef1ce5376f7c7f0ec4f594905247285d0e07422d
andy-shi88 <shi88.andy@gmail.com>
2018-02-22 14:08:41 +0700

    #9 add middleware to protect route

---
+++ src/helpers/auth.js
@@ -0,0 +1,10 @@
+import VueCookie from 'vue-cookie';
+
+export default (to, from, next) => {
+	const token = VueCookie.get('token');
+	if (token) {
+		next();
+	} else {
+		next({ path: '/' })
+	}
+}
