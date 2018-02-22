a8dc30d7ccdd2e641010368499234d50d5530c16
andy-shi88 <shi88.andy@gmail.com>
2018-02-22 14:09:04 +0700

    #9 implement the auth middleware

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import Login from '../components/Login/Login';
 import ProposalList from '../components/ProposalList/ProposalList';
 import UserList from '../components/UserList/UserList';
+import auth from '../helpers/auth';
 
 Vue.use(Router);
 
@@ -17,11 +18,13 @@ export default new Router({
       path: '/proposal',
       name: 'ProposalList',
       component: ProposalList,
+      beforeEnter: auth,
     },
     {
       path: '/user',
       name: 'UserList',
       component: UserList,
+      beforeEnter: auth,
     },
   ],
 });
