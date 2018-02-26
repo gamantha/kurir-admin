9eb772c664be1e1cc025ea236d50c115b866a377
Bhirawa Mbani <bhirmbani@gmail.com>
2018-02-26 14:00:46 +0700

    change base url to local

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_API_URL = 'https://kurirbackend-dev.herokuapp.com/api/';
+export const BASE_API_URL = 'http://localhost:3000/api/';
 export const ROLES = {
   SYSADMIN: 'sysadmin',
   SITEADMIN: 'siteadmin',
