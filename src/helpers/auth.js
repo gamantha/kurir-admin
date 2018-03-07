73118fbccfc47d1a8aa5e5958c12df0e6f9a0218
Bhirawa Mbani <bhirmbani@gmail.com>
2018-03-07 09:39:39 +0700

    adding bothAdmin check in auth helper

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -20,3 +20,15 @@ export const siteAdminCheck = (to, from, next) => {
     next({ path: '/unauthorized' });
   }
 };
+
+export const bothAdmin = (to, from, next) => {
+  const token = VueCookie.get('token');
+  const userRole = Helper.parseToken(token).role;
+  if (token && userRole === 'siteadmin') {
+    next();
+  } else if (token && userRole === 'sysadmin') {
+    next();
+  } else {
+    next({ path: '/unauthorized' });
+  }
+};
