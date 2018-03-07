32bcf7c53339c63c6d35aa7176e7bca9fecdf416
Bhirawa Mbani <bhirmbani@gmail.com>
2018-03-07 09:30:31 +0700

    adding unauthorized and 404 page

---
+++ src/components/FourOhFour/FourOhFour.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="main">
+    <h1>404</h1>
+    <h3>We're thinking you are lost in the middle of the internet.</h3>
+    <p>It doesn't matter though. Maybe go to <router-link to="/">login page?</router-link></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FourOhFour',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+#main {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin: -100px 0 0 -200px;
+  z-index: 15;
+}
+</style>
---
+++ src/components/Unauthorized/Unauthorized.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="main">
+    <h1>You are not authorized to access this page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ItemList',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+#main {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin: -60px 0 0 -270px;
+  z-index: 15;
+}
+</style>
