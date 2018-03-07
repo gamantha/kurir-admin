71e5541f3fe65b2e427d96bb5135683be5a6aaaa
Bhirawa Mbani <bhirmbani@gmail.com>
2018-03-07 09:29:55 +0700

    jwt helper

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,8 +1,13 @@
 import moment from 'moment';
+import jwt from 'jsonwebtoken';
 
 class Helpers {
   static decodeDate(time) {
     return moment(time).format('D MMM YYYY');
+  }
+
+  static parseToken(token) {
+    return jwt.verify(token, 'asdsa');
   }
 }
 
