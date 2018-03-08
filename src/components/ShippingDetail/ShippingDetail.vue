3733c143cb42f8bc8293177657cacb118b6e156c
Bhirawa Mbani <bhirmbani@gmail.com>
2018-03-08 16:21:29 +0700

    using vues store instead of route params

--- src/components/ShippingDetail/ShippingDetail.vue
+++ src/components/ShippingDetail/ShippingDetail.vue
@@ -3,7 +3,9 @@
     <sui-table celled striped>
       <sui-table-header>
         <sui-table-row>
-          <sui-table-headerCell colspan="4">Shipping Detail for package number {{ this.detail.ticketNumber }}</sui-table-headerCell>
+          <sui-table-headerCell colspan="4">
+            Shipping Detail for package number {{ this.getSelectedItem.ticketNumber }}
+          </sui-table-headerCell>
         </sui-table-row>
       </sui-table-header>
 
@@ -12,57 +14,57 @@
           <sui-table-cell collapsing>
             <sui-icon name="envelope" /> Sender email
           </sui-table-cell>
-          <sui-table-cell colspan="3">{{ this.detail.Sender.User.email }}</sui-table-cell>
+          <sui-table-cell colspan="3">{{ this.getSelectedItem.Sender.User.email }}</sui-table-cell>
           <!-- <sui-table-cell collapsing text-align="right">10 hours ago</sui-table-cell> -->
         </sui-table-row>
         <sui-table-row>
           <sui-table-cell collapsing>
             <sui-icon name="archive" /> Package definition
           </sui-table-cell>
-          <sui-table-cell colspan="3">{{ this.detail.name }}</sui-table-cell>
+          <sui-table-cell colspan="3">{{ this.getSelectedItem.name }}</sui-table-cell>
           <!-- <sui-table-cell text-align="right">10 hours ago</sui-table-cell> -->
         </sui-table-row>
         <sui-table-row>
           <sui-table-cell collapsing>
             <sui-icon name="user" /> Receiver name
           </sui-table-cell>
-          <sui-table-cell colspan="3">{{ this.detail.Receiver.name }}</sui-table-cell>
+          <sui-table-cell colspan="3">{{ this.getSelectedItem.Receiver.name }}</sui-table-cell>
           <!-- <sui-table-cell text-align="right">10 hours ago</sui-table-cell> -->
         </sui-table-row>
         <sui-table-row>
           <sui-table-cell collapsing>
             <sui-icon name="phone" /> Receiver phone
           </sui-table-cell>
-          <sui-table-cell colspan="3">{{ this.detail.Receiver.phone }}</sui-table-cell>
+          <sui-table-cell colspan="3">{{ this.getSelectedItem.Receiver.phone }}</sui-table-cell>
           <!-- <sui-table-cell text-align="right">10 hours ago</sui-table-cell> -->
         </sui-table-row>
         <sui-table-row>
           <sui-table-cell>
             <sui-icon name="balance scale" /> Weight
           </sui-table-cell>
-          <sui-table-cell colspan="3">{{ this.detail.weight }} Kilogram</sui-table-cell>
+          <sui-table-cell colspan="3">{{ this.getSelectedItem.weight }} Kilogram</sui-table-cell>
           <!-- <sui-table-cell text-align="right">10 hours ago</sui-table-cell> -->
         </sui-table-row>
         <sui-table-row>
           <sui-table-cell>
             <sui-icon name="home" /> Origin
           </sui-table-cell>
-          <sui-table-cell colspan="3">{{ this.detail.from }}</sui-table-cell>
+          <sui-table-cell colspan="3">{{ this.getSelectedItem.from }}</sui-table-cell>
           <!-- <sui-table-cell text-align="right">10 hours ago</sui-table-cell> -->
         </sui-table-row>
         <sui-table-row>
           <sui-table-cell>
             <sui-icon name="paper plane" /> Destination
           </sui-table-cell>
-          <sui-table-cell colspan="3">{{ this.detail.to }}</sui-table-cell>
+          <sui-table-cell colspan="3">{{ this.getSelectedItem.to }}</sui-table-cell>
           <!-- <sui-table-cell text-align="right">10 hours ago</sui-table-cell> -->
         </sui-table-row>
         <sui-table-row>
           <sui-table-cell>
             <sui-icon name="briefcase" /> Courier email
           </sui-table-cell>
-          <sui-table-cell v-if="this.detail.Courier" colspan="3">{{ 
-            this.detail.Courier.User.email
+          <sui-table-cell v-if="this.getSelectedItem.Courier" colspan="3">{{ 
+            this.getSelectedItem.Courier.User.email
           }}</sui-table-cell>
           <sui-table-cell v-else colspan="3">
             <strong>Item ini belum punya kurir</strong>
@@ -73,23 +75,23 @@
           <sui-table-cell>
             <sui-icon name="money" /> Item price for sender
           </sui-table-cell>
-          <sui-table-cell colspan="3">Rp. {{ this.detail.cost }}</sui-table-cell>
+          <sui-table-cell colspan="3">Rp. {{ this.getSelectedItem.cost }}</sui-table-cell>
           <!-- <sui-table-cell text-align="right">10 hours ago</sui-table-cell> -->
         </sui-table-row>
         <sui-table-row>
           <sui-table-cell collapsing>
             <sui-icon name="dollar sign" /> Reward value for Courier
           </sui-table-cell>
-          <sui-table-cell colspan="3">Rp. {{ this.detail.reward }}</sui-table-cell>
+          <sui-table-cell colspan="3">Rp. {{ this.getSelectedItem.reward }}</sui-table-cell>
           <!-- <sui-table-cell text-align="right">10 hours ago</sui-table-cell> -->
         </sui-table-row>
         <sui-table-row>
           <sui-table-cell collapsing>
             <sui-icon name="truck" /> Package status
           </sui-table-cell>
-          <sui-table-cell colspan="2">{{ this.detail.status }}</sui-table-cell>
+          <sui-table-cell colspan="2">{{ this.getSelectedItem.status }}</sui-table-cell>
           <sui-table-cell text-align="right">
-            Last updated: {{ this.decodeDate(this.detail.updatedAt) }} / <strong>{{ this.fromDate(this.detail.updatedAt) }}</strong>
+            Last updated: {{ this.decodeDate(this.getSelectedItem.updatedAt) }} / <strong>{{ this.fromDate(this.getSelectedItem.updatedAt) }}</strong>
           </sui-table-cell>
         </sui-table-row>
       </sui-table-body>
@@ -98,6 +100,7 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import Helper from '../../helpers';
 
 export default {
@@ -117,11 +120,15 @@ export default {
   },
   created() {
     this.detail = this.$route.params;
-    if (!this.detail.name) {
+    // go to shipping list if data not available
+    if (this.getSelectedItem.length < 1) {
       this.$router.push({
         name: 'ShippingList',
       });
     }
+  },
+  computed: {
+    ...mapGetters(['getSelectedItem']),
   },
 };
 </script>
